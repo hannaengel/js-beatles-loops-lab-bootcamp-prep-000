3c65578655bc8061bc6c351dd839a1b69dc36f31
Hanna Engel <hengel@alumni.berklee.edu>
2019-03-31 19:46:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-
-const arrayConst= ['const']
-var oldArray = ['var']
-// newArray = []
-
-// newArray = newArrayConst;
-
 function theBeatlesPlay (musicians, instruments){
   newArray= []
-  for (i=0;i<=musicians.length; i++){
-    newArray.push(`$(musicians[i]) plays $(instruments[i]).`)
-  }
+  for (i=0;i<=musicians.length-1; i++){
+    newArray.push(`${musicians[i]} plays ${instruments[i]}.`)
 }return newArray
-  
+}
