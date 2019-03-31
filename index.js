a64badb13a136bbac08c062093a9ad6de793deea
Hanna Engel <hengel@alumni.berklee.edu>
2019-03-31 19:47:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ function theBeatlesPlay (musicians, instruments){
   newArray= []
   for (i=0;i<=musicians.length-1; i++){
     newArray.push(`${musicians[i]} plays ${instruments[i]}.`)
-}
-}
-return newArray
+  }
+  return newArray
 }
